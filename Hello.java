d23dceb86797acab653eb3c5db386601ae75d5a1
AngelaMHarris <112001804+AngelaMHarris@users.noreply.github.com>
2022-08-24 11:47:48 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/************
+* Lab 01
+*****
+* Angela Harris
+* 08-24-22
+* CMSC-255-004
+******/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Angela Harris>");
+		System.out.println("<Lab 01>");
+		System.out.println("<Practice creating and running a Java program>");
+		System.out.println("<CMSC-255-004>");
+		System.out.println();
+	}
+} 
